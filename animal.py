bb41d4ed6090bc7a64bd1564f7268f0cb88cff91
Wagner, J <35028309+dota247@users.noreply.github.com>
2021-03-26 09:53:18 -0700

    Add files via upload

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Juan A Wagner
+# Date  : 3/26/2021
+# Description: Script asks a user to type in their favorite animal,
+#              Then returns their input along with a message.
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + '.')
